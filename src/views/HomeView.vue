9268db5a11c7a814de6db6619886a9e260421569
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-08-29 04:29:05 +0700

    add tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-red-400 text-xl">Hello</h1>
   </main>
 </template>
